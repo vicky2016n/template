7f30356c880adae8b3cb5e68e81f15e2dab474a9
vicky2016n <vickynehra2016@gmail.com>
2023-05-27 00:28:19 +0530

    first commit

---
+++ js/durgesh.js
@@ -0,0 +1,19 @@
+$(document).ready(function (){
+
+    $(".menus li a").click(function (e){
+
+        let target = $(this).attr("href");
+
+        $("html,body").animate(
+        
+            {
+                scrollTop: $(target).offset().top
+
+            },
+            1000
+        );
+
+        e.preventDefault();
+    });
+
+});
